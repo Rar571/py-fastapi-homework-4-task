65087ee9dd8f2e6d3bd238269834b7e76f3be79a
Ruslan <stadnikruslan168@gmail.com>
2026-06-15 20:08:39 +0300

    Solution

--- src/routes/profiles.py
+++ src/routes/profiles.py
@@ -14,32 +14,32 @@ router = APIRouter()
 
 @router.post("/users/{user_id}/profile", response_model=ProfileCreateSchema)
 async def create_user_profile(
-                              user_id: int,
-                              user_data: ProfileCreateSchema,
-                              db: AsyncSession = Depends(get_db),
-                              token: str = Depends(get_token),
-                              avatar: UploadFile = File(None),
-                              jwt_manager = Depends(get_jwt_auth_manager),
-                              s3_client: S3StorageInterface = Depends(get_s3_storage_client)
+    user_id: int,
+    user_data: ProfileCreateSchema,
+    db: AsyncSession = Depends(get_db),
+    token: str = Depends(get_token),
+    avatar: UploadFile = File(None),
+    jwt_manager=Depends(get_jwt_auth_manager),
+    s3_client: S3StorageInterface = Depends(get_s3_storage_client),
 ):
     try:
         payload = jwt_manager.decode_access_token(token)
     except (TokenExpiredError, InvalidTokenError):
         raise HTTPException(status_code=401, detail="Token has expired.")
     token_user_id = payload.get("sub")
-    result = await db.execute(select(UserModel).where(
-        UserModel.id == user_id
-    ))
+    result = await db.execute(select(UserModel).where(UserModel.id == user_id))
     user = result.scalar_one_or_none()
     if token_user_id != user_id and user.group.name != "admin":
-        raise HTTPException(status_code=403, detail="You don't have permission to edit this profile.")
+        raise HTTPException(
+            status_code=403, detail="You don't have permission to edit this profile."
+        )
     if not user:
         raise HTTPException(status_code=401, detail="User not found or not active.")
     if not user.is_active:
         raise HTTPException(status_code=401, detail="User not found or not active.")
-    result_profile = await db.execute(select(UserProfileModel).where(
-        UserProfileModel.user_id == token_user_id
-    ))
+    result_profile = await db.execute(
+        select(UserProfileModel).where(UserProfileModel.user_id == token_user_id)
+    )
     profile = result_profile.scalar_one_or_none()
     if profile:
         raise HTTPException(status_code=400, detail="User already has a profile.")
@@ -47,20 +47,19 @@ async def create_user_profile(
     try:
         if avatar:
             avatar_filename = f"{user_id}_avatar.jpg"
-            await s3_client.upload_file(
-                avatar_filename,
-                avatar.file.read()
-            )
+            await s3_client.upload_file(avatar_filename, avatar.file.read())
             avatar_url = f"http://minio-theater/avatars/{avatar_filename}"
     except Exception:
-        raise HTTPException(status_code=500, detail="Failed to upload avatar. Please try again later.")
+        raise HTTPException(
+            status_code=500, detail="Failed to upload avatar. Please try again later."
+        )
     new_profile = UserProfileModel(
         first_name=user_data.first_name,
         last_name=user_data.last_name,
         gender=user_data.gender,
         date_of_birth=user_data.date_of_birth,
         info=user_data.info,
-        avatar=avatar_url
+        avatar=avatar_url,
     )
     db.add(new_profile)
     await db.commit()
--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -1,40 +1,48 @@
 from datetime import date
-from fastapi import UploadFile, Form, File, HTTPException
-from pydantic import BaseModel, field_validator, HttpUrl
+from pydantic import BaseModel, field_validator
 from database.models.accounts import GenderEnum
 from validation import (
     validate_name,
-    validate_image,
     validate_gender,
-    validate_birth_date
+    validate_birth_date,
 )
+
 
 class ProfileCreateSchema(BaseModel):
     first_name: str
+
     @field_validator("first_name")
     @classmethod
     def check_english_letters(cls, first_name: str):
         validate_name(first_name)
         return first_name
+
     last_name: str
+
     @field_validator("last_name")
     @classmethod
-    def check_english_letters(cls, last_name: str):
+    def check_english_letters_31(cls, last_name: str):
         validate_name(last_name)
         return last_name
+
     gender: GenderEnum
+
     @field_validator("gender")
     @classmethod
     def check_gender(cls, gender: str):
         validate_gender(gender)
         return gender
+
     date_of_birth: date
+
     @field_validator("date_of_birth")
     @classmethod
     def check_date_of_birth(cls, date_of_birth: date):
         validate_birth_date(date_of_birth)
         return date_of_birth
+
     info: str
+
     @field_validator("info")
     @classmethod
     def check_info(cls, info: str):
